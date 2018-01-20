5556f15e3ebbccd65c166be6813c3c049633f795
BuildTools <robbereygel97@gmail.com>
2018-01-20 17:08:56 +0100

    Added foreground FCM notifications

---
+++ app/src/main/java/com/example/fabiovandooren/runningapp/MyFirebaseInstanceIdService.java
@@ -0,0 +1,20 @@
+package com.example.fabiovandooren.runningapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by gebruiker on 20/01/2018.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String REG_TOKEN = "REG_TOKEN";
+    @Override
+    public void onTokenRefresh() {
+
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN, recent_token);
+    }
+}
